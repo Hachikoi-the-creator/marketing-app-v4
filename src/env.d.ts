6e35ffa3638f8387b19b925c80e7950302469ac5
hachikoi-the-creator <facka72@gmail.com>
2025-04-13 14:34:12 -0600

    Fix: build issues

---
+++ src/env.d.ts
@@ -0,0 +1,15 @@
+interface ImportMetaEnv {
+  readonly ADMIN_KEY: string;
+  readonly API_KEY: string;
+  readonly AUTH_DOMAIN: string;
+  readonly PROJECT_ID: string;
+  readonly STORAGE_BUCKET: string;
+  readonly MESSAGING_SENDER_ID: string;
+  readonly APP_ID: string;
+  readonly MEASUREMENT_ID: string;
+  readonly DATABASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,32 +2,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-import {
-  getSecret,
-  API_KEY,
-  AUTH_DOMAIN,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGING_SENDER_ID,
-  APP_ID,
-  MEASUREMENT_ID,
-  DATABASE_URL,
-} from "astro:env/server";
 
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: getSecret(API_KEY),
-  authDomain: getSecret(AUTH_DOMAIN),
-  projectId: getSecret(PROJECT_ID),
-  storageBucket: getSecret(STORAGE_BUCKET),
-  messagingSenderId: getSecret(MESSAGING_SENDER_ID),
-  appId: getSecret(APP_ID),
-  measurementId: getSecret(MEASUREMENT_ID),
-  databaseURL: getSecret(DATABASE_URL),
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+  measurementId: import.meta.env.MEASUREMENT_ID,
+  databaseURL: import.meta.env.DATABASE_URL,
 };
 
 // Initialize Firebase
